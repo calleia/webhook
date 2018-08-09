84c1051c5cb1c125211dc73f09293780d2621d5b
Fellipe Calleia <fellipe@calleia.com.br>
2018-08-09 11:41:50 -0300

    Create WebhookServer class

--- webhookd.py
+++ webhookd.py
@@ -4,20 +4,17 @@ import json
 import time
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
-HOST = "0.0.0.0"
-PORT = 80
-
-
-def parse_json(self):
-    try:
-        j = json.loads(self)
-    except:
-        print("ERRO")
-    else:
-        print(j)
-
 
 class RequestHandler(BaseHTTPRequestHandler):
+
+    def parse_json(self, string=None):
+        try:
+            decoded_string = string.decode("utf-8")
+            parsed_json = json.loads(decoded_string)
+        except:
+            print("There was a problem parsing JSON.")
+        else:
+            print(parsed_json)
 
     def do_HEAD(self):
         self.send_response(200)
@@ -34,31 +31,38 @@ class RequestHandler(BaseHTTPRequestHandler):
         self.wfile.write(response)
 
     def do_POST(self):
-
         content_len = int(self.headers["content-length"])
         post_body = self.rfile.read(content_len)
 
         self.send_response(200)
         self.end_headers()
 
-        parse_json(post_body)
+        self.parse_json(post_body)
 
         return
 
 
+class WebhookServer():
+
+    def __init__(self, port=80, address="0.0.0.0"):
+
+        server_address = (address, port)
+
+        self.server = HTTPServer(server_address, RequestHandler)
+
+    def start(self):
+        print(time.asctime(), "Server started @ %s:%s"
+              % self.server.server_address)
+
+        try:
+            self.server.serve_forever()
+        except KeyboardInterrupt:
+            self.server.server_close()
+
+        print(time.asctime(), "Server stopped @ %s:%s"
+              % self.server.server_address)
+
+
 if __name__ == "__main__":
-
-    server_address = (HOST, PORT)
-
-    httpd = HTTPServer(server_address, RequestHandler)
-
-    print(time.asctime(), "Server started @ %s:%s" % server_address)
-
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        pass
-
-    httpd.server_close()
-
-    print(time.asctime(), "Server stopped @ %s:%s" % server_address)
+    server = WebhookServer()
+    server.start()
